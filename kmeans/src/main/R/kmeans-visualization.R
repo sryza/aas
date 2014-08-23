d84944986e94baae672f888d274e80dd5be55afc
Sean Owen <srowen@gmail.com>
2014-08-23 17:36:49 +0100

    Add copyrigh to R code

--- kmeans/src/main/R/kmeans-visualization.R
+++ kmeans/src/main/R/kmeans-visualization.R
@@ -1,18 +1,29 @@
+# Copyright 2014 Sandy Ryza, Josh Wills, Sean Owen, Uri Laserson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 install.packages("rgl") # First time only
 library(rgl)
 
 # Read clusters and data separately
-clusters_data <-
-  read.csv(pipe("hdfs dfs -cat /user/spark/sample/*"))
+clusters_data <- read.csv(pipe("hdfs dfs -cat /user/spark/sample/*"))
 clusters <- clusters_data[1]
 data <- data.matrix(clusters_data[-c(1)])
 rm(clusters_data)
 
 # Make a random 3D projection and normalize
 random_projection <- matrix(data = rnorm(3*ncol(data)), ncol = 3)
-random_projection_norm <-
-  random_projection /
-    sqrt(rowSums(random_projection*random_projection))
+random_projection_norm <- random_projection / sqrt(rowSums(random_projection*random_projection))
 
 # Project and make a new data frame
 projected_data <- data.frame(data %*% random_projection_norm)
