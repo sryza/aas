60227d74b0af50a663d6014e86293663c9dc31e0
Sean Owen <srowen@gmail.com>
2015-01-31 17:25:46 +0000

    Remove usages of Guava

--- ch07-graph/src/main/scala/com/cloudera/datascience/graph/RunGraph.scala
+++ ch07-graph/src/main/scala/com/cloudera/datascience/graph/RunGraph.scala
@@ -6,9 +6,10 @@
 
 package com.cloudera.datascience.graph
 
-import com.cloudera.datascience.common.XmlInputFormat
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
 
-import com.google.common.hash.Hashing
+import com.cloudera.datascience.common.XmlInputFormat
 
 import org.apache.hadoop.io.{Text, LongWritable}
 import org.apache.hadoop.conf.Configuration
@@ -196,7 +197,15 @@ object RunGraph extends Serializable {
   }
 
   def hashId(str: String): Long = {
-    Hashing.md5().hashString(str).asLong()
+    val bytes = MessageDigest.getInstance("MD5").digest(str.getBytes(StandardCharsets.UTF_8))
+    (bytes(0) & 0xFFL) |
+    ((bytes(1) & 0xFFL) << 8) |
+    ((bytes(2) & 0xFFL) << 16) |
+    ((bytes(3) & 0xFFL) << 24) |
+    ((bytes(4) & 0xFFL) << 32) |
+    ((bytes(5) & 0xFFL) << 40) |
+    ((bytes(6) & 0xFFL) << 48) |
+    ((bytes(7) & 0xFFL) << 56)
   }
 
   def chiSq(YY: Int, YB: Int, YA: Int, T: Long): Double = {
