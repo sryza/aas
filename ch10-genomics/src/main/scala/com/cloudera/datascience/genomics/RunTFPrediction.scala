39c0ba359747f346840f6c42222643a283fa520e
Uri Laserson <uri.laserson@gmail.com>
2017-02-11 18:02:44 -0500

    Updates example for newest version of ADAM

--- ch10-genomics/src/main/scala/com/cloudera/datascience/genomics/RunTFPrediction.scala
+++ ch10-genomics/src/main/scala/com/cloudera/datascience/genomics/RunTFPrediction.scala
@@ -7,43 +7,37 @@
 package com.cloudera.datascience.genomics
 
 import java.io.File
+import java.nio.file.Paths
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.bdgenomics.utils.parquet.io.LocalFileByteAccess
-import org.bdgenomics.adam.models.ReferenceRegion
+import org.bdgenomics.adam.models.{Alphabet, ReferenceRegion}
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.bdgenomics.adam.rdd.BroadcastRegionJoin
-import org.bdgenomics.adam.util.{TwoBitFile, SequenceUtils}
-import org.bdgenomics.formats.avro.Feature
-import org.bdgenomics.adam.rich.ReferenceMappingContext._
-
-import scala.annotation.tailrec
+import org.bdgenomics.adam.rdd.{InnerShuffleRegionJoin, LeftOuterShuffleRegionJoin}
+import org.bdgenomics.adam.util.TwoBitFile
+import org.bdgenomics.utils.io.LocalFileByteAccess
 
 object RunTFPrediction {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().setAppName("TF Prediction"))
 
+    // configuration
+    val hdfsPrefix = "/user/ds/genomics"
+    val localPrefix = "/user/ds/genomics"
+
+    // Set up broadcast variables for computing features along with some
+    // utility functions
+
     // Load the human genome reference sequence
     val bHg19Data = sc.broadcast(
       new TwoBitFile(
         new LocalFileByteAccess(
-          new File("/user/ds/genomics/hg19.2bit"))))
+          new File(Paths.get(localPrefix, "hg19.2bit").toString))))
 
-    val phylopRDD = sc.adamLoad[Feature, Nothing]("/user/ds/genomics/phylop")
-      // clean up a few irregularities in the phylop data
-      .filter(f => f.getStart <= f.getEnd)
-
-    val tssRDD = sc.loadFeatures("/user/ds/genomics/gencode.v18.annotation.gtf")
-      .filter(_.getFeatureType == "transcript")
-      .map(f => (f.getContig.getContigName, f.getStart))
-
-    val bTssData = sc.broadcast(tssRDD
-      // group by contig name
-      .groupBy(_._1)
-      // create Vector of TSS sites for each chromosome
-      .map(p => (p._1, p._2.map(_._2.toLong).toVector))
-      // collect into local in-memory structure for broadcasting
-      .collect().toMap)
+    // fn for finding closest transcription start site
+    // naive; exercise for reader: make this faster
+    def distanceToClosest(loci: Vector[Long], query: Long): Long = {
+      loci.map(x => math.abs(x - query)).min
+    }
 
     // CTCF PWM from https://dx.doi.org/10.1016/j.cell.2012.12.009
     // generated with genomics/src/main/python/pwm.py
@@ -66,122 +60,102 @@ object RunTFPrediction {
       Map('A'->0.1520,'C'->0.2820,'G'->0.0082,'T'->0.5578),
       Map('A'->0.3644,'C'->0.3105,'G'->0.2125,'T'->0.1127)))
 
-
-    // Define some utility functions
-
-    // fn for finding closest transcription start site
-    // naive...make this better
-    def distanceToClosest(loci: Vector[Long], query: Long): Long = {
-      loci.map(x => math.abs(x - query)).min
-    }
-
     // compute a motif score based on the TF PWM
     def scorePWM(ref: String): Double = {
-      val score1 = ref.sliding(bPwmData.value.length).map { s =>
-        s.zipWithIndex.map(p => bPwmData.value(p._2)(p._1)).product
-      }.max
-      val rc = SequenceUtils.reverseComplement(ref)
-      val score2 = rc.sliding(bPwmData.value.length).map(s => {
-        s.zipWithIndex.map(p => bPwmData.value(p._2)(p._1)).product
-      }).max
+      val score1 = (ref.sliding(bPwmData.value.length)
+        .map(s => {
+          s.zipWithIndex.map(p => bPwmData.value(p._2)(p._1)).product})
+        .max)
+      val rc = Alphabet.dna.reverseComplementExact(ref)
+      val score2 = (rc.sliding(bPwmData.value.length)
+        .map(s => {
+          s.zipWithIndex.map(p => bPwmData.value(p._2)(p._1)).product})
+        .max)
       math.max(score1, score2)
     }
 
-    // functions for labeling the DNase peaks as binding sites or not; compute
-    // overlaps between an interval and a set of intervals
-    // naive impl - this only works because we know the ChIP-seq peaks are non-
-    // overlapping (how do we verify this? exercise for the reader)
-    def isOverlapping(i1: (Long, Long), i2: (Long, Long)) = (i1._2 > i2._1) && (i1._1 < i2._2)
+    // build in-memory structure for computing distance to TSS
+    // we are essentially manually implementing a broadcast join here
+    val tssRDD = (sc.loadFeatures(Paths.get(hdfsPrefix, "gencode.v18.annotation.gtf").toString).rdd
+      .filter(_.getFeatureType == "transcript")
+      .map(f => (f.getContigName, f.getStart)))
+    // this broadcast variable will hold the broadcast side of the "join"
+    val bTssData = sc.broadcast(tssRDD
+      // group by contig name
+      .groupBy(_._1)
+      // create Vector of TSS sites for each chromosome
+      .map(p => (p._1, p._2.map(_._2.toLong).toVector))
+      // collect into local in-memory structure for broadcasting
+      .collect().toMap)
 
-    def isOverlappingLoci(loci: Vector[(Long, Long)], testInterval: (Long, Long)): Boolean = {
-      @tailrec
-      def search(m: Int, M: Int): Boolean = {
-        val mid = m + (M - m) / 2
-        if (M <= m) {
-          false
-        } else if (isOverlapping(loci(mid), testInterval)) {
-          true
-        } else if (testInterval._2 <= loci(mid)._1) {
-          search(m, mid)
-        } else {
-          search(mid + 1, M)
-        }
-      }
-      search(0, loci.length)
-    }
+    // load conservation data; independent of cell line
+    val phylopRDD = (sc.loadParquetFeatures(Paths.get(hdfsPrefix, "phylop").toString).rdd
+      // clean up a few irregularities in the phylop data
+      .filter(f => f.getStart <= f.getEnd)
+      .map(f => (ReferenceRegion.unstranded(f), f)))
+
+    // MAIN LOOP
 
     val cellLines = Vector("GM12878", "K562", "BJ", "HEK293", "H54", "HepG2")
 
-    val dataByCellLine = cellLines.map { cellLine =>
-      val dnaseRDD = sc.loadFeatures(
-        s"/user/ds/genomics/dnase/$cellLine.DNase.narrowPeak")
-      val chipseqRDD = sc.loadFeatures(
-        s"/user/ds/genomics/chip-seq/$cellLine.ChIP-seq.CTCF.narrowPeak")
+    val dataByCellLine = cellLines.map(cellLine => {
+      val dnaseRDD = (sc.loadFeatures(Paths.get(hdfsPrefix, s"dnase/$cellLine.DNase.narrowPeak").toString).rdd
+        .map(f => ReferenceRegion.unstranded(f)).map(r => (r, r)))
 
-      // generate the fn for labeling the data points
-      val bBindingData = sc.broadcast(chipseqRDD
-        // group peaks by chromosome
-        .groupBy(_.getContig.getContigName) // RDD[(String, Iterable[Feature])]
-        // for each chr, for each ChIP-seq peak, extract start and end
-        .map(p => (p._1, p._2.map(f => (f.getStart: Long, f.getEnd: Long)))) // RDD[(String, Iterable[(Long, Long)])]
-        // for each chr, sort the peaks (we know they're not overlapping)
-        .map(p => (p._1, p._2.toVector.sortBy(x => x._1))) // RDD[(String, Vector[(Long, Long)])]
-        // collect them back into a local in-memory data structure for broadcasting
-        .collect().toMap)
+      // add label (TF bound is true, unbound is false)
+      val chipseqRDD = (sc.loadFeatures(Paths.get(hdfsPrefix, s"chip-seq/$cellLine.ChIP-seq.CTCF.narrowPeak").toString).rdd
+        .map(f => ReferenceRegion.unstranded(f)).map(r => (r, r)))
+      val dnaseWithLabelRDD = (LeftOuterShuffleRegionJoin(bHg19Data.value.sequences, 1000000, sc)
+        .partitionAndJoin(dnaseRDD, chipseqRDD)
+        .map(p => (p._1, p._2.size))
+        .reduceByKey(_ + _)
+        .map(p => (p._1, p._2 > 0))
+        .map(p => (p._1, p)))
 
-      def generateLabel(f: Feature) = {
-        val contig = f.getContig.getContigName
-        if (!bBindingData.value.contains(contig)) {
-          false
-        } else {
-          val testInterval = (f.getStart: Long, f.getEnd: Long)
-          isOverlappingLoci(bBindingData.value(contig), testInterval)
-        }
+      // add conservation data
+      def aggPhylop(values: Vector[Double]) = {
+        val avg = values.sum / values.length
+        val m = values.min
+        val M = values.max
+        (avg, m, M)
       }
+      val dnaseWithPhylopRDD = (LeftOuterShuffleRegionJoin(bHg19Data.value.sequences, 1000000, sc)
+        .partitionAndJoin(dnaseRDD, phylopRDD)
+        .filter(!_._2.isEmpty)
+        .map(p => (p._1, p._2.get.getScore.doubleValue))
+        .groupByKey()
+        .map(p => (p._1, aggPhylop(p._2.toVector))))
 
-      // join the DNase peak data with conservation data to generate those features
-      val dnaseWithPhylopRDD = BroadcastRegionJoin.partitionAndJoin(sc, dnaseRDD, phylopRDD)
-        // group the conservation values by DNase peak
-        .groupBy(x => x._1.getFeatureId)
-        // compute conservation stats on each peak
-        .map { x =>
-          val y = x._2.toSeq
-          val peak = y(0)._1
-          val values = y.map(_._2.getValue)
-          // compute phylop features
-          val avg = values.reduce(_ + _) / values.length
-          val m = values.max
-          val M = values.min
-          (peak.getFeatureId, peak, avg, m, M)
-        }
-
-      dnaseWithPhylopRDD.map { tup =>
-        val peak = tup._2
-        val featureId = peak.getFeatureId
-        val contig = peak.getContig.getContigName
-        val start = peak.getStart
-        val end = peak.getEnd
-        val score = scorePWM(bHg19Data.value.extract(ReferenceRegion(peak)))
-        val avg = tup._3
-        val m = tup._4
-        val M = tup._5
-        val closest_tss = math.min(
-          distanceToClosest(bTssData.value(contig), peak.getStart),
-          distanceToClosest(bTssData.value(contig), peak.getEnd))
-        val tf = "CTCF"
-        val line = cellLine
-        val bound = generateLabel(peak)
-        (featureId, contig, start, end, score, avg, m, M, closest_tss, tf, line, bound)
-      }
-    }
+      // build final training example RDD
+      val examplesRDD = (InnerShuffleRegionJoin(bHg19Data.value.sequences, 1000000, sc)
+        .partitionAndJoin(dnaseWithLabelRDD, dnaseWithPhylopRDD)
+        .map(tup => (tup._1, tup._2, bHg19Data.value.extract(tup._1._1)))
+        .filter(!_._3.contains("N"))
+        .map(tup => {
+          val region = tup._1._1
+          val label = tup._1._2
+          val contig = region.referenceName
+          val start = region.start
+          val end = region.end
+          val phylopAvg = tup._2._1
+          val phylopMin = tup._2._2
+          val phylopMax = tup._2._3
+          val seq = tup._3
+          val pwmScore = scorePWM(seq)
+          val closestTss = math.min(
+            distanceToClosest(bTssData.value(contig), start),
+            distanceToClosest(bTssData.value(contig), end))
+          val tf = "CTCF"
+          (contig, start, end, pwmScore, phylopAvg, phylopMin, phylopMax, closestTss, tf, cellLine, label)}))
+      examplesRDD
+    })
 
     // union the prepared data together
     val preTrainingData = dataByCellLine.reduce(_ ++ _)
     preTrainingData.cache()
-    preTrainingData.count() // 801263
-    preTrainingData.filter(_._12 == true).count() // 220285
+    preTrainingData.count() // 802059
+    preTrainingData.filter(_._11 == true).count() // 220344
 
     // carry on into classification
-
   }
 }
