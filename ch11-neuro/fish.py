b221dc2c6a856d75c9e417b359d570e025c94a34
Uri Laserson <uri.laserson@gmail.com>
2017-02-15 01:05:44 -0500

    Updated ch11 neuro

--- ch11-neuro/fish.py
+++ ch11-neuro/fish.py
@@ -8,57 +8,59 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-plt.ion()
+import seaborn as sns
+
+import thunder as td
 
 ##################
 # data exploration
 ##################
 
 # load some zebrafish brain data
-path_to_images = ('path/to/thunder/python/thunder/utils/data/fish/tif-stack')
-imagesRDD = tsc.loadImages(path_to_images, inputformat='tif-stack')
+data = td.images.fromtif('/user/ds/neuro/fish', engine=sc)
 
 # explore the resulting object
-print imagesRDD
-print imagesRDD.rdd
-print imagesRDD.first()
-print imagesRDD.first()[1].shape
-print imagesRDD.dims
-print imagesRDD.nimages
+print data
+print data.values
+print data.values.first()
+print data.values._rdd
+print data.values._rdd.first()
+print data.shape
+print data.count()
 
 # plot the raw data
-img = imagesRDD.values().first()
+img = data.first()
 plt.imshow(img[:, : ,0], interpolation='nearest', aspect='equal', cmap='gray')
 
 # plot subsampled data
-subsampled = imagesRDD.subsample((5, 5, 1))
+subsampled = data.subsample((5, 5, 1))
 plt.imshow(subsampled.first()[1][:, : ,0], interpolation='nearest', aspect='equal', cmap='gray')
-print subsampled.dims
+print subsampled.shape
 
 # reshuffle data to series representation
-seriesRDD = imagesRDD.toSeries()
-print seriesRDD.dims
-print seriesRDD.index
-print seriesRDD.count()
-print seriesRDD.rdd.takeSample(False, 1, 0)[0]
-print seriesRDD.max()
+series = data.toseries()
+print series.shape
+print series.index
+print series.count()
+print series.values._rdd.takeSample(False, 1)[0]
+print series.max().values
 
 # distributed computation of stats
-stddevRDD = seriesRDD.seriesStdev()
-print stddevRDD.take(3)
-print stddevRDD.dims
+stddev = series.map(lambda s: s.std())
+print stddev.values._rdd.take(3)
+print stddev.shape
 
 # collecting data locally and repacking it
-repacked = stddevRDD.pack()
+repacked = stddev.toarray()
 plt.imshow(repacked[:,:,0], interpolation='nearest', cmap='gray', aspect='equal')
 print type(repacked)
 print repacked.shape
 
 # plot some of the time series themselves
-plt.plot(seriesRDD.center().subset(50).T)
+plt.plot(series.center().sample(50).toarray().T)
 
 # distributed computatino of custom statistics
-seriesRDD.apply(lambda x: x.argmin())
+series.map(lambda x: x.argmin())
 
 
 ###############################
@@ -66,26 +68,28 @@ seriesRDD.apply(lambda x: x.argmin())
 ###############################
 
 import numpy as np
-from thunder import KMeans
+from pyspark.mllib.clustering import KMeans
 
-seriesRDD = tsc.loadSeries('path/to/thunder/python/thunder/utils/data/fish/bin')
-print seriesRDD.dims
-print seriesRDD.index
+images = td.images.frombinary(
+    '/user/ds/neuro/fish-long', order='F', engine=sc)
+series = images.toseries()
 
-normalizedRDD = seriesRDD.normalize(baseline='mean')
-stddevs = (normalizedRDD
-    .seriesStdev()
-    .values()
-    .sample(False, 0.1, 0)
-    .collect())
-plt.hist(stddevs, bins=20)
-plt.plot(normalizedRDD.subset(50, thresh=0.1, stat='std').T)
+normalized = series.normalize(method='mean')
+stddevs = (normalized
+    .map(lambda s: s.std())
+    .sample(1000))
+plt.hist(stddevs.values, bins=20)
+plt.plot(
+    normalized
+        .filter(lambda s: s.std() >= 0.1)
+        .sample(50)
+        .values.T)
 
 # perform k-means on the normalized series
 ks = [5, 10, 15, 20, 30, 50, 100, 200]
 models = []
 for k in ks:
-    models.append(KMeans(k=k).fit(normalizedRDD))
+    models.append(KMeans.train(normalized.values._rdd.values(), k))
 
 # define a couple functions to score the clustering quality
 def model_error_1(model):
@@ -94,11 +98,14 @@ def model_error_1(model):
         center = model.centers[cluster_id]
         diff = center - series
         return diff.dot(diff) ** 0.5
-    
-    return normalizedRDD.apply(series_error).sum()
+
+    return (normalized
+        .map(series_error)
+        .toarray()
+        .sum())
 
 def model_error_2(model):
-    return 1. / model.similarity(normalizedRDD).sum()
+    return model.computeCost(normalized.values._rdd.values())
 
 # compute the error metrics for the different resulting clusterings
 errors_1 = np.asarray(map(model_error_1, models))
@@ -109,9 +116,11 @@ plt.plot(
 
 # plot the best performing model
 model20 = models[3]
-plt.plot(model20.centers.T)
+plt.plot(np.asarray(model20.centers).T)
 
 # finally, plot each brain region according to its characteristic behavior
-by_cluster = model20.predict(normalizedRDD).pack()
+from matplotlib.colors import ListedColormap
 cmap_cat = ListedColormap(sns.color_palette("hls", 10), name='from_list')
-plt.imshow(by_cluster[:, :, 0], interpolation='nearest', aspect='equal', cmap='gray')
+by_cluster = normalized.map(lambda s: model20.predict(s)).toarray()
+plt.imshow(by_cluster[:, :, 0], interpolation='nearest',
+    aspect='equal', cmap='gray')
