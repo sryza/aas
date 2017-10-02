06f5746e0a1e0a2c58963ae8ac3b11a3f50dc1dc
Sean Owen <sowen@cloudera.com>
2017-10-02 09:26:55 +0100

    Issue #113: set checkpoint dir for ALS

--- ch03-recommender/src/main/scala/com/cloudera/datascience/recommender/RunRecommender.scala
+++ ch03-recommender/src/main/scala/com/cloudera/datascience/recommender/RunRecommender.scala
@@ -18,6 +18,8 @@ object RunRecommender {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().getOrCreate()
+    // Optional, but may help avoid errors due to long lineage
+    spark.sparkContext.setCheckpointDir("hdfs:///tmp/")
 
     val base = "hdfs:///user/ds/"
     val rawUserArtistData = spark.read.textFile(base + "user_artist_data.txt")
