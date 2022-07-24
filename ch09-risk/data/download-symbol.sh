905afc47aafd569ee2a42f52946556451f9a9165
analyticalmonk <akashtndn.acm@gmail.com>
2022-07-24 19:06:13 +0530

    remove material from 2nd edition

--- ch09-risk/data/download-symbol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copyright 2015 and onwards Sanford Ryza, Uri Laserson, Sean Owen and Joshua Wills
-#
-# See LICENSE file for further information.
-
-wget -O ${2}/${1}.csv "http://www.google.com/finance/historical?q=${1}&startdate=Jan+01%2C+2000&enddate=Dec+31%2C+2013&output=csv"
--- ch11-neuro/README.md
+++
@@ -1,2 +0,0 @@
-The data in the `fish-long/` subdirectory come from version 0.4.1 of the
-Thunder project, which is Apache v2 licensed.
